b5e6ca6bd0fc9d1f04f21ca5b39cca3ef68babf4
Rachmat Ardico Perdana <rahmatardiko99@gmail.com>
2022-10-31 20:51:47 +0700

    Formatter

--- config/logging.php
+++ config/logging.php
@@ -1,5 +1,6 @@
 <?php
 
+use Monolog\Formatter\JsonFormatter;
 use Monolog\Handler\NullHandler;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\SyslogUdpHandler;
@@ -103,7 +104,7 @@ return [
             'driver' => 'monolog',
             'level' => env('LOG_LEVEL', 'debug'),
             'handler' => StreamHandler::class,
-            'formatter' => env('LOG_STDERR_FORMATTER'),
+            'formatter' => JsonFormatter::class,
             'with' => [
                 'stream' => storage_path("logs/application.log"),
             ],
